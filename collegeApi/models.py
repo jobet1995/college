6750aadc300d9fe6074fd39c78a85fe2c0356b87
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-17 14:45:40 +0000

    feat: Add API views for enrollment list and details endpoints, add admin configuration for college API, add model for college API with course and emergency contact relationships, add tests for college API module and add views for college API module

---
+++ collegeApi/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from course.models import Course
+from emergency_contact.models import Emergency
+# Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class collegeApi(BaseModel):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    emergency = models.ForeignKey(Emergency, on_delete=models.CASCADE)
--- enrollment/views.py
+++ enrollment/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Enrollment
+from .serializers import EnrollmentSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def enrollment_list(request):
+    if request.method == 'GET':
+        data = Enrollment.objects.all()
+        serializer = EnrollmentSerializer(
+            data, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = EnrollmentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTPS_500_BAD_REQUEST)
+
+
+@api_view(['PUT', 'DELETE'])
+def enrollment_details(request, pk):
+    try:
+        enrollment = Enrollment.objects.get(pk=pk)
+    except Enrollment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = EnrollmentSerializer(
+            enrollment, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        enrollment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
