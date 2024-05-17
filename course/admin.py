19e24064c76ae98437f201185297846e152b08e6
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-05-17 11:18:58 +0000

    added comments

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,9 @@
+"""
+@description: A Django administration module for the course app.
+@author: Jobet P. Casquejo
+@last modified date: 5-17-2024
+@version: 1.0.0
+"""
 from django.contrib import admin
 
 # Register your models here.
--- course/tests.py
+++ course/tests.py
@@ -1,3 +1,9 @@
+"""
+@description: A Django administration module for the course app.
+@author: Jobet P. Casquejo
+@last modified date: 5-17-2024
+@version: 1.0.0
+"""
 from django.test import TestCase
 
 # Create your tests here.
