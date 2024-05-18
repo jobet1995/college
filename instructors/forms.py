d9924d51410ec4f3797331f42823095607f3f3c7
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 13:05:59 +0000

    feat: Changed model from Department to Instructor

--- instructors/forms.py
+++ instructors/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from .models import Instructor
 from department.models import Department
 
+
 class InstructorForm(forms.ModelForm):
     department = forms.ModelChoiceField(queryset=Department.objects.all())
 
     class Meta:
-        model = Department
+        model = Instructor
         fields = '__all__'
