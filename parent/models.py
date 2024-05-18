54cb1cd6a59071ab975c0abf6d7fa687f957ba96
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 01:47:25 +0000

    feat: Added models for parent and financial information with fields for mother, father, and financial details and updated views to include GET and POST methods for parent list

--- parent/models.py
+++ parent/models.py
@@ -10,6 +10,48 @@ class BaseModel(models.Model):
 
 
 class Parent(BaseModel):
+    print("Name of Mother or Guardian")
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     job = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    email = models.EmailField()
+    print("Address of Mother")
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=10)
+    print("Name of Father")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    job = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    email = models.EmailField()
+    print("Address of Father")
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f{self.first_name} {self.last_name}
+
+
+class FinancialInformation(BaseModel):
+    print("Financial Information")
+    print("Are you a dependent of your Parents?")
+    YES_NO_CHOICES = [
+        (True, "Yes"),
+        (False, "No")
+    ]
+    are_you_dependent = models.BooleanField(choices=YES_NO_CHOICES)
+    your_family_joint_annual_income = models.DecimalField(
+        max_digits=10, decimal_places=2)
+    working = models.TextField()
+    applied_scholarship = models.BooleanField(choices=YES_NO_CHOICES)
+    information = models.TextField()
+    financial_aid = models.BooleanField(choices=YES_NO_CHOICES)
+    information = models.TextField()
+
+    def __str__(self):
+        return f{information.self}
--- parent/views.py
+++ parent/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Parent, FinancialInformation
+from .serializer import ParentSerializer, FinancialInformationSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def parent_list(request):
+  if request.method == 'GET':
+    data = Parent.objects.all()
+    serializer = ParentSerializer(data, context={'request': request}, many=True)
+    return Response(serializer.data)
+  elif request.method == 'POST':
+    serializer = ParentSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
