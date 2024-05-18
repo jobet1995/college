56cda5055d667ac8c0f1d7ea0802274831f69a3a
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 10:55:38 +0000

    feat: Add BaseModel class with created_at and updated_at fields and add Student class with enrollment, date_signed_by_applicant, and applicant_signature fields

--- student/models.py
+++ student/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from enrollment import Enrollment
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Student(BaseModel):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    date_signed_by_applicant = models.DateField()
+    applicant_signature = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.enrollment
