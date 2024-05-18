78fd4d90842198629d6649448d2efb6e231d2735
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 08:02:55 +0000

    feat: Add REST API endpoints for College API

--- collegeApi/views.py
+++ collegeApi/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import collegeApi
+from .serializers import collegeApiSerializer
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def college_api_list(request):
+    if request.method == 'GET':
+        data = collegeApi.objects.all()
+        serializer = collegeApiSerializer(
+            data, context={'request': request}, many=True
+        )
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = collegeApiSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTPS_500_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def college_api_details(request, pk):
+    try:
+        college = collegeApi.objects.get(pk=pk)
+    except collegeApi.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        data = collegeApi.objects.all()
+        serializer = collegeApiSerializer(
+            data, context={'request': request}, many=True
+        )
+    elif request.method == 'PUT':
+        serializer = collegeApiSerializer(
+            college, data=request.data, context={'request': request}
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_500_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        college.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
