c197fd31634f45326b91d3ddd587abe12f9bd33c
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 13:05:25 +0000

    feat: Add handling of form data

--- instructors/views.py
+++ instructors/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import Instructor
 from .serializers import InstructorSerializer
+from .forms import InstructorForm
 
 
 @api_view(['GET', 'POST'])
@@ -24,9 +25,12 @@ def instructor_list(request):
         if not created:
             return Response({"message": "Instructor already exists."}, status=status.HTTP_409_CONFLICT)
         serializer = InstructorSerializer(data=request.data)
+        form = InstructorForm(request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_201_CREATED)
+        elif form.is_valid():
+            form.save()
         return Response(serializer.errors, status=status.HTTPS_400_BAD_REQUEST)
 
 
@@ -38,7 +42,8 @@ def instructor_details(request, pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         data = Instructor.objects.all()
-        serializer = InstructorSerializer(data, context={'request':request}, many=True)
+        serializer = InstructorSerializer(
+            data, context={'request': request}, many=True)
         return Response(serializer.data)
     elif request.method == 'PUT':
         serializer = InstructorSerializer(
@@ -46,8 +51,12 @@ def instructor_details(request, pk):
             data=request.data,
             context={'request': request}
         )
+        form = InstructorForm(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        elif form.is_valid():
+            form.save()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
