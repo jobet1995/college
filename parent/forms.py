1f44c93fe080c9dcbf5ef8da8aa49dafe64aadbd
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 13:19:10 +0000

    feat: Add FinancialInformation model and create FinancialInformationForm

--- parent/forms.py
+++ parent/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Parent
+from .models import Parent, FinancialInformation
 
 
 class ParentForm(forms.ModelForm):
     class Meta:
         model = Parent
+        fields = '__all__'
+
+
+class FinancialInformationForm(forms.ModelForm):
+    class Meta:
+        model = FinancialInformation
         fields = '__all__'
