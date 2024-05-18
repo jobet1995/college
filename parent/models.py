cc102df4d44cbdf987361c11e31102db8f4f97e8
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 01:14:25 +0000

    feat: Add created_at and updated_at fields to the BaseModel, add abstract Meta class to BaseModel and add Parent model with first_name field

--- parent/models.py
+++ parent/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 
 class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Parent(BaseModel):
+    first_name = models.CharField()
