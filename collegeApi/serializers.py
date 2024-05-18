a19236fcb0b5851238a23493b169b52281399c79
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 08:25:36 +0000

    fix: Fix serializers.py to handle exception for ValidationError

--- collegeApi/serializers.py
+++ collegeApi/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import collegeApi
 from course.models import Course
-from emergency_contact.models import Emergency
+# from emergency_contact.models import Emergency
 from parent.models import Parent
 from instructors.models import Instructor
 from enrollment.models import Enrollment
@@ -9,16 +9,21 @@ from subjects.models import Subject
 
 
 class collegeApiSerializer(serializers.ModelSerializer):
-    course = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all())
-    emergency_contact = serializers.PrimaryKeyRelatedField(
-        queryset=Emergency.objects.all())
-    parent = serializers.PrimaryKeyRelatedField(queryset=Parent.objects.all())
-    instructor = serializers.PrimaryKeyRelatedField(
-        queryset=Instructor.objects.all())
-    enrollment = serializers.PrimaryKeyRelatedField(
-        queryset=Enrollment.objects.all())
-    subject = serializers.PrimaryKeyRelatedField(
-        queryset=Subject.objects.all())
+    try:
+        course = serializers.PrimaryKeyRelatedField(
+            queryset=Course.objects.all())
+        # emergency_contact = serializers.PrimaryKeyRelatedField(
+        # queryset=Emergency.objects.all())
+        parent = serializers.PrimaryKeyRelatedField(
+            queryset=Parent.objects.all())
+        instructor = serializers.PrimaryKeyRelatedField(
+            queryset=Instructor.objects.all())
+        enrollment = serializers.PrimaryKeyRelatedField(
+            queryset=Enrollment.objects.all())
+        subject = serializers.PrimaryKeyRelatedField(
+            queryset=Subject.objects.all())
+    except serializers.ValidationError as e:
+        print(f"Errors: {e}")
 
     class Meta:
         model = collegeApi
