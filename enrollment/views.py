acbcf1fd82cbb0a27f6e4ea42d17eabfa8dc43ce
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-05-18 13:38:16 +0000

    fix: Fix indentation issue in enrollment views.py file

--- enrollment/views.py
+++ enrollment/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import Enrollment
 from .serializers import EnrollmentSerializer
+from .forms import EnrollmentForm
 
 
 @api_view(['GET', 'POST'])
@@ -15,8 +16,12 @@ def enrollment_list(request):
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = EnrollmentSerializer(data=request.data)
+        form = EnrollmentForm(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        elif form.is_valid():
+            form.save()
             return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTPS_500_BAD_REQUEST)
 
@@ -31,8 +36,12 @@ def enrollment_details(request, pk):
     if request.method == 'PUT':
         serializer = EnrollmentSerializer(
             enrollment, data=request.data, context={'request': request})
+        form = EnrollmentForm(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        elif form.is_valid():
+            form.save()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
